ef8cbcf7a09b29d9f7f27a90ba149716fd378653
asdasd571 <kng955626@gmail.com>
2025-02-11 11:39:28 +0900

    data offer 수정 완료

--- swagger_server/controllers/all_data_offers_controller.py
+++ swagger_server/controllers/all_data_offers_controller.py
@@ -1,20 +1,27 @@
 import connexion
 import six
 
-from swagger_server.models.data_offer_info import DataOfferInfo  # noqa: E501
+from swagger_server.models import DataOfferInfo  # noqa: E501
 from swagger_server import util
 
 
 def offers_post(body):  # noqa: E501
     """offers_post
 
-    Allows to create a new data offer # noqa: E501
+    Allows to create a new data offer
 
     :param body: 
     :type body: dict | bytes
 
     :rtype: DataOfferInfo
     """
-    if connexion.request.is_json:
-        body = DataOfferInfo.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+    if isinstance(body, dict):  # body가 dict 타입일 경우
+        # DataOfferInfo 객체 생성
+        try:
+            data_offer_info = DataOfferInfo.from_dict(body)
+            # 데이터 처리 후 응답 반환
+            return jsonify(data_offer_info.to_dict()), 201
+        except KeyError as e:
+            return jsonify({"error": f"Missing key: {str(e)}"}), 400
+    else:
+        return jsonify({"error": "Invalid input, JSON required."}), 400
--- swagger_server/controllers/individual_data_offer_controller.py
+++ swagger_server/controllers/individual_data_offer_controller.py
@@ -4,14 +4,23 @@ import six
 from swagger_server import util
 
 
-def offers_data_offer_id_delete(data_offer_id):  # noqa: E501
+def offers_data_offer_id_delete(data_offer_id):
     """offers_data_offer_id_delete
 
-    To delete the data offer # noqa: E501
+    To delete the data offer
 
-    :param data_offer_id: 
+    :param data_offer_id: The identifier of the data offer to be deleted.
     :type data_offer_id: str
 
     :rtype: None
     """
-    return 'do some magic!'
+    # 데이터 제공이 존재하는지 확인
+    if data_offer_id in data_offers:
+        # 데이터 제공 삭제
+        del data_offers[data_offer_id]
+        
+        # 204 No Content 응답 코드와 함께 성공적으로 삭제됨
+        return '', 204
+    else:
+        # 데이터 제공이 존재하지 않으면 404 Not Found 응답 코드 반환
+        return jsonify({"error": "Data offer not found"}), 404
