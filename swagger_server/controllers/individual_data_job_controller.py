d7dfbc5f0f3f0e308ece13d3a66ce10cf47aeaa5
asdasd571 <kng955626@gmail.com>
2025-02-11 11:02:02 +0900

    수정

--- swagger_server/controllers/individual_data_job_controller.py
+++ swagger_server/controllers/individual_data_job_controller.py
@@ -4,17 +4,29 @@ import six
 from swagger_server.models.data_job_id import DataJobId  # noqa: E501
 from swagger_server import util
 
+from flask import jsonify
+from models import DataJobId  # DataJobId 모델을 가져오는 가정
+from database import db  # 데이터베이스 관련 객체 (예시)
 
 def data_jobs_data_job_id_delete(data_job_id):  # noqa: E501
     """data_jobs_data_job_id_delete
 
-    To delete the created data job # noqa: E501
+    To delete the created data job.
 
-    :param data_job_id: 
+    :param data_job_id: The ID of the data job to delete.
     :type data_job_id: dict | bytes
 
     :rtype: None
     """
     if connexion.request.is_json:
+        # JSON 요청 본문을 DataJobId 객체로 변환
         data_job_id = DataJobId.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        
+        # 데이터 작업 삭제 로직 (예: 데이터베이스에서 해당 작업을 삭제)
+        # db.delete_data_job(data_job_id)  # 실제 삭제 로직을 구현해야 함
+
+        # 예시: 삭제가 성공적으로 이루어졌다면, 204 No Content 응답을 반환
+        return '', 204  # HTTP 204 No Content 상태 코드 반환
+
+    # JSON 형식이 아닐 경우 잘못된 요청 처리
+    return jsonify({"error": "Invalid input, JSON expected"}), 400
--- swagger_server/models/data_delivery_mechanism.py
+++ swagger_server/models/data_delivery_mechanism.py
@@ -15,16 +15,16 @@ class DataDeliveryMechanism(Model):
 
     Do not edit the class manually.
     """
-    def __init__(self, data_delivery_method: Object=None, kafka_delivery_configuration: KafkaDeliveryConfiguration=None):  # noqa: E501
+    def __init__(self, data_delivery_method: object=None, kafka_delivery_configuration: KafkaDeliveryConfiguration=None):  # noqa: E501
         """DataDeliveryMechanism - a model defined in Swagger
 
         :param data_delivery_method: The data_delivery_method of this DataDeliveryMechanism.  # noqa: E501
-        :type data_delivery_method: Object
+        :type data_delivery_method: object
         :param kafka_delivery_configuration: The kafka_delivery_configuration of this DataDeliveryMechanism.  # noqa: E501
         :type kafka_delivery_configuration: KafkaDeliveryConfiguration
         """
         self.swagger_types = {
-            'data_delivery_method': Object,
+            'data_delivery_method': object,
             'kafka_delivery_configuration': KafkaDeliveryConfiguration
         }
 
@@ -47,24 +47,24 @@ class DataDeliveryMechanism(Model):
         return util.deserialize_model(dikt, cls)
 
     @property
-    def data_delivery_method(self) -> Object:
+    def data_delivery_method(self) -> object:
         """Gets the data_delivery_method of this DataDeliveryMechanism.
 
         Delivery Method supported  # noqa: E501
 
         :return: The data_delivery_method of this DataDeliveryMechanism.
-        :rtype: Object
+        :rtype: object
         """
         return self._data_delivery_method
 
     @data_delivery_method.setter
-    def data_delivery_method(self, data_delivery_method: Object):
+    def data_delivery_method(self, data_delivery_method: object):
         """Sets the data_delivery_method of this DataDeliveryMechanism.
 
         Delivery Method supported  # noqa: E501
 
         :param data_delivery_method: The data_delivery_method of this DataDeliveryMechanism.
-        :type data_delivery_method: Object
+        :type data_delivery_method: object
         """
         if data_delivery_method is None:
             raise ValueError("Invalid value for `data_delivery_method`, must not be `None`")  # noqa: E501
